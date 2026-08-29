888a40db348a28e5aa6e54d721387181f2757a96
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-29 20:27:21 -0300

    feat(drone): add mission statuses

---
+++ backend/src/main/java/com/rotacerta/api/model/DroneMissionStatus.java
@@ -0,0 +1,15 @@
+package com.rotacerta.api.model;
+
+public enum DroneMissionStatus {
+    PLANNED,
+    AUTHORIZED,
+    LOADING,
+    READY_FOR_TAKEOFF,
+    IN_FLIGHT,
+    APPROACHING,
+    LOWERING_PACKAGE,
+    DELIVERED,
+    RETURNING,
+    COMPLETED,
+    ABORTED
+}
