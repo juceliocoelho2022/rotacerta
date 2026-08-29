3e1c410d12cce69fbab6fc9c9daa41fbb95b06f5
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-29 11:41:34 -0300

    style: load dashboard, deliveries and routes themes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
 import './styles/operations.css'
+import './styles/executive.css'
+import './styles/deliveries.css'
+import './styles/routes.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
